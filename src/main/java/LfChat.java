af4e8d8fef29cb65cb13bafea6b05417e5f25a57
LF1928 <zhanglifei.lifei@gmail.com>
2025-01-23 00:14:51 +0800

    Add greeting

--- src/main/java/LfChat.java
+++ src/main/java/LfChat.java
@@ -7,5 +7,11 @@ public class LfChat {
                 + " | |____| |      | |___ | | | | (_| |_ \n"
                 + " |______|_|      |_____||_| |_|\\__,_(_)\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm LFChat");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }
