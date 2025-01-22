9d42befbe34b7304f1ad19d65f2edb35fe88065c
LF1928 <zhanglifei.lifei@gmail.com>
2025-01-23 00:13:58 +0800

    Rename main class and logo

---
+++ src/main/java/LfChat.java
@@ -0,0 +1,11 @@
+public class LfChat {
+    public static void main(String[] args) {
+        String logo = "  _      ______   _____ _           _   \n"
+                + " | |    |  ____| |  __ || |         | |  \n"
+                + " | |    | |__    | |    | |__   __| |  \n"
+                + " | |    |  __|   | |    | '_ \\ / _` |  \n"
+                + " | |____| |      | |___ | | | | (_| |_ \n"
+                + " |______|_|      |_____||_| |_|\\__,_(_)\n";
+        System.out.println("Hello from\n" + logo);
+    }
+}
