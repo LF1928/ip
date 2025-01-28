42c4c21c96c7b547a61ef39c6b7674f62391a7cb
__ZLF__\zhang <zhanglifei.lifei@gmail.com>
2025-01-28 16:21:47 +0800

    Add enum and reorganise

--- src/main/java/LfChat.java
+++ src/main/java/LfChat.java
@@ -3,7 +3,9 @@ import java.util.ArrayList;
 
 
 public class LfChat {
-    public static void main(String[] args) throws MissingDescriptionException{
+    private static ArrayList<Task> listOfTasks = new ArrayList<>();
+
+    public static void main(String[] args) throws MissingDescriptionException {
         String logo = "  _      ______   _____ _           _\n"
                 + " | |    |  ____| |  __ || |         | |\n"
                 + " | |    | |__    | |    | |__   __| |\n"
@@ -11,96 +13,120 @@ public class LfChat {
                 + " | |____| |      | |___ | | | | (_| |_\n"
                 + " |______|_|      |_____||_| |_|\\__,_(_)\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("____________________________________________________________");
+        printLine();
         System.out.println(" Hello! I'm LFChat");
         System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
+        printLine();
 
         Scanner scanner = new Scanner(System.in);
         String userInput;
-        ArrayList<Task> listOfTasks = new ArrayList<>();
+
 
         while (true) {
             userInput = scanner.nextLine();
-            Task task = new Task(userInput);
+            Task task;
+            String commandWord = userInput.split(" ")[0].toUpperCase();
 
-            if (userInput.equalsIgnoreCase("bye")) {
-                bye();
-                break;
-            }
+            try {
+                Command command = Command.valueOf(commandWord);
+                command.execute(userInput);
 
-            if (userInput.equalsIgnoreCase("list")) {
-                listTasks(listOfTasks);
-                continue;
-            }
-
-            if (userInput.startsWith("mark ")) {
-                int taskNumber = Integer.parseInt(userInput.split(" ")[1]);
-                markAsDone(listOfTasks, taskNumber);
-                continue;
-            }
-
-            if (userInput.startsWith("unmark ")) {
-                int taskNumber = Integer.parseInt(userInput.split(" ")[1]);
-                markAsUndone(listOfTasks, taskNumber);
-                continue;
-            }
-
-            if (userInput.startsWith("todo ")) {
-                String description = userInput.substring(5);
-                task = new ToDos(description);
-                addPrintTask(listOfTasks, task);
-                continue;
-            }
-
-            if (userInput.startsWith("deadline ")) {
-                String description = userInput.substring(9, userInput.indexOf("/by")).trim();
-                String deadline = userInput.substring(userInput.indexOf("/by") + 4).trim();
-                task = new Deadlines(description, deadline);
-                addPrintTask(listOfTasks, task);
-                continue;
-            }
-
-            if (userInput.startsWith("event ")) {
-                String description = userInput.substring(6, userInput.indexOf("/from")).trim();
-                String startTime = userInput.substring(userInput.indexOf("/from") + 6, userInput.indexOf("/to")).trim();
-                String endTime = userInput.substring(userInput.indexOf("/to") + 4).trim();
-                task = new Events(description, startTime, endTime);
-                addPrintTask(listOfTasks, task);
-                continue;
-            }
-
-            if (userInput.startsWith("delete ")) {
-                int taskNumber = Integer.parseInt(userInput.split(" ")[1]);
-                deleteTask(listOfTasks, taskNumber);
-                continue;
-            }
-
-            else {
-                System.out.println("____________________________________________________________");
+                if (command == Command.BYE) {
+                    break;
+                }
+            } catch (MissingDescriptionException e) {
+                printLine();
+                System.out.println(e.getMessage());
+                printLine();
+            } catch (IllegalArgumentException e) {
+                printLine();
                 System.out.println(" Sorry, the command does not exist :<");
-                System.out.println("____________________________________________________________");
+                printLine();
             }
-
 
         }
 
         scanner.close();
 
     }
+    enum Command {
+        BYE {
+            @Override
+            void execute(String input) {
+                bye();
+            }
+        },
+        LIST {
+            @Override
+            void execute(String input) {
+                listTasks(listOfTasks);
+            }
+        },
+        MARK {
+            @Override
+            void execute(String input) {
+                int taskNumber = Integer.parseInt(input.split(" ")[1]);
+                markAsDone(listOfTasks, taskNumber);
+            }
+        },
+        UNMARK {
+            @Override
+            void execute(String input) {
+                int taskNumber = Integer.parseInt(input.split(" ")[1]);
+                markAsUndone(listOfTasks, taskNumber);
+            }
+        },
+        TODO {
+            @Override
+            void execute(String input) throws MissingDescriptionException{
+                String description = input.substring(5).trim();
+                Task task = new ToDos(description);
+                addPrintTask(listOfTasks, task);
+            }
+        },
+        DEADLINE {
+            @Override
+            void execute(String input) throws MissingDescriptionException{
+                String description = input.substring(9, input.indexOf("/by")).trim();
+                String deadline = input.substring(input.indexOf("/by") + 4).trim();
+                Task task = new Deadlines(description, deadline);
+                addPrintTask(listOfTasks, task);
+            }
+        },
+        EVENT {
+            @Override
+            void execute(String input) throws MissingDescriptionException{
+                String description = input.substring(6, input.indexOf("/from")).trim();
+                String startTime = input.substring(input.indexOf("/from") + 6, input.indexOf("/to")).trim();
+                String endTime = input.substring(input.indexOf("/to") + 4).trim();
+                Task task = new Events(description, startTime, endTime);
+                addPrintTask(listOfTasks, task);
+            }
+        },
+        DELETE {
+            @Override
+            void execute(String input) {
+                int taskNumber = Integer.parseInt(input.split(" ")[1]);
+                deleteTask(listOfTasks, taskNumber);
+            }
+        };
+        abstract void execute(String input) throws MissingDescriptionException;
+
+    }
+
     public static void listTasks(ArrayList<Task> listOfTasks) {
-        System.out.println("____________________________________________________________");
+        printLine();
         System.out.println(" Here are the tasks in your list:");
         for (int i = 0; i < listOfTasks.size(); i++) {
             System.out.println(" " + (i + 1) + "." + listOfTasks.get(i).toString());
         }
-        System.out.println("____________________________________________________________");
+        printLine();
     }
 
     public static void bye() {
-        System.out.println("____________________________________________________________");
+        printLine();
         System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        printLine();
     }
 
     public static void markAsDone(ArrayList<Task> listOfTasks, int taskNumber) {
@@ -111,10 +137,10 @@ public class LfChat {
 
         listOfTasks.get(taskNumber - 1).markAsDone();
 
-        System.out.println("____________________________________________________________");
+        printLine();
         System.out.println("Nice! I've marked this task as done:");
         System.out.println("  " + listOfTasks.get(taskNumber - 1).toString());
-        System.out.println("____________________________________________________________");
+        printLine();
     }
 
     public static void deleteTask(ArrayList<Task> listOfTasks, int taskNumber) {
@@ -122,12 +148,12 @@ public class LfChat {
             System.out.println("Invalid task number!");
             return;
         }
-        System.out.println("____________________________________________________________");
+        printLine();
         System.out.println("Noted. I've removed this task:");
         System.out.println("  " + listOfTasks.get(taskNumber - 1).toString());
         listOfTasks.remove(taskNumber - 1);
         System.out.println(" Now you have " + listOfTasks.size() + " tasks in the list.");
-        System.out.println("____________________________________________________________");
+        printLine();
     }
     public static void markAsUndone(ArrayList<Task> listOfTasks, int taskNumber) {
         if (taskNumber <= 0 || taskNumber > listOfTasks.size()) {
@@ -137,10 +163,10 @@ public class LfChat {
 
         listOfTasks.get(taskNumber - 1).markAsUndone();
 
-        System.out.println("____________________________________________________________");
+        printLine();
         System.out.println("OK, I've marked this task as not done yet:");
         System.out.println("  " + listOfTasks.get(taskNumber - 1).toString());
-        System.out.println("____________________________________________________________");
+        printLine();
     }
 
     public static void addPrintTask(ArrayList<Task> listOfTasks, Task task) throws MissingDescriptionException {
@@ -148,10 +174,13 @@ public class LfChat {
             throw new MissingDescriptionException("Description for the tasks cannot be empty.");
         }
         listOfTasks.add(task);
-        System.out.println("____________________________________________________________");
+        printLine();
         System.out.println(" Got it. I've added this task:");
         System.out.println("  " + task.toString());
         System.out.println(" Now you have " + listOfTasks.size() + " tasks in the list.");
+        printLine();
+    }
+    public static void printLine() {
         System.out.println("____________________________________________________________");
     }
 
