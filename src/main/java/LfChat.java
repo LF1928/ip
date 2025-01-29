cb26624cb06b31cb68297bf431bb9e3d9b8ee1c2
__ZLF__\zhang <zhanglifei.lifei@gmail.com>
2025-01-29 11:39:26 +0800

    Add more OOP

--- src/main/java/LfChat.java
+++ src/main/java/LfChat.java
@@ -10,225 +10,32 @@ import java.util.ArrayList;
 
 public class LfChat {
     private static ArrayList<Task> listOfTasks = new ArrayList<>();
-    private static final String FILE_PATH = "data/LFChat.txt";
 
-    public static void main(String[] args) throws MissingDescriptionException {
-        String logo = "  _      ______   _____ _           _\n"
-                + " | |    |  ____| |  __ || |         | |\n"
-                + " | |    | |__    | |    | |__   __| |\n"
-                + " | |    |  __|   | |    | '_ \\ / _` |\n"
-                + " | |____| |      | |___ | | | | (_| |_\n"
-                + " |______|_|      |_____||_| |_|\\__,_(_)\n";
-        System.out.println("Hello from\n" + logo);
-        printLine();
-        System.out.println(" Hello! I'm LFChat");
-        System.out.println(" What can I do for you?");
-        printLine();
-
+    public static void main(String[] args) {
+        Ui.start();
         Scanner scanner = new Scanner(System.in);
         String userInput;
-        EnsureDirectoryExists();
-        loadTasksFromFile();
-
+        Storage.EnsureDirectoryExists();
+        Storage.loadTasksFromFile(listOfTasks);
 
 
         while (true) {
             userInput = scanner.nextLine();
-            Task task;
-            String commandWord = userInput.split(" ")[0].toUpperCase();
-
             try {
-                Command command = Command.valueOf(commandWord);
-                command.execute(userInput);
+                Command command = Parser.parse(userInput);
+                command.execute(userInput, listOfTasks);
 
                 if (command == Command.BYE) {
                     break;
                 }
-            } catch (MissingDescriptionException e) {
-                printLine();
+            } catch (MissingDescriptionException | IllegalArgumentException |InvalidTaskNumberException e) {
+                Ui.printLine();
                 System.out.println(e.getMessage());
-                printLine();
-            } catch (IllegalArgumentException e) {
-                printLine();
-                System.out.println(" Sorry, the command does not exist :<");
-                printLine();
+                Ui.printLine();
             }
-
         }
-        saveTasksToFile();
+        Storage.saveTasksToFile(listOfTasks);
         scanner.close();
 
     }
-    enum Command {
-        BYE {
-            @Override
-            void execute(String input) {
-                bye();
-            }
-        },
-        LIST {
-            @Override
-            void execute(String input) {
-                listTasks(listOfTasks);
-            }
-        },
-        MARK {
-            @Override
-            void execute(String input) {
-                int taskNumber = Integer.parseInt(input.split(" ")[1]);
-                markAsDone(listOfTasks, taskNumber);
-            }
-        },
-        UNMARK {
-            @Override
-            void execute(String input) {
-                int taskNumber = Integer.parseInt(input.split(" ")[1]);
-                markAsUndone(listOfTasks, taskNumber);
-            }
-        },
-        TODO {
-            @Override
-            void execute(String input) throws MissingDescriptionException{
-                String description = input.substring(5).trim();
-                Task task = new ToDos(description);
-                addPrintTask(listOfTasks, task);
-            }
-        },
-        DEADLINE {
-            @Override
-            void execute(String input) throws MissingDescriptionException{
-                String description = input.substring(9, input.indexOf("/by")).trim();
-                String deadline = DateTimeParse.extractDateTime(input);
-                Task task = new Deadlines(description, deadline);
-                addPrintTask(listOfTasks, task);
-            }
-        },
-        EVENT {
-            @Override
-            void execute(String input) throws MissingDescriptionException{
-                String description = input.substring(6, input.indexOf("/from")).trim();
-                String[] startEndTime = DateTimeParse.extractStartAndEndTimes(input);
-                Task task = new Events(description, startEndTime);
-                addPrintTask(listOfTasks, task);
-            }
-        },
-        DELETE {
-            @Override
-            void execute(String input) {
-                int taskNumber = Integer.parseInt(input.split(" ")[1]);
-                deleteTask(listOfTasks, taskNumber);
-            }
-        };
-        abstract void execute(String input) throws MissingDescriptionException;
-
-    }
-
-    public static void listTasks(ArrayList<Task> listOfTasks) {
-        printLine();
-        System.out.println(" Here are the tasks in your list:");
-        for (int i = 0; i < listOfTasks.size(); i++) {
-            System.out.println(" " + (i + 1) + "." + listOfTasks.get(i).toString());
-        }
-        printLine();
-    }
-
-    public static void bye() {
-        printLine();
-        System.out.println(" Bye. Hope to see you again soon!");
-        printLine();
-    }
-
-    public static void markAsDone(ArrayList<Task> listOfTasks, int taskNumber) {
-        if (taskNumber <= 0 || taskNumber > listOfTasks.size()) {
-            System.out.println("Invalid task number!");
-            return;
-        }
-        Task task = listOfTasks.get(taskNumber - 1);
-        listOfTasks.get(taskNumber - 1).markAsDone();
-
-        printLine();
-        System.out.println("Nice! I've marked this task as done:");
-        System.out.println("  " + task.toString());
-        printLine();
-    }
-
-    public static void markAsUndone(ArrayList<Task> listOfTasks, int taskNumber) {
-        if (taskNumber <= 0 || taskNumber > listOfTasks.size()) {
-            System.out.println("Invalid task number!");
-            return;
-        }
-        Task task = listOfTasks.get(taskNumber - 1);
-        listOfTasks.get(taskNumber - 1).markAsUndone();
-
-        printLine();
-        System.out.println("OK, I've marked this task as not done yet:");
-        System.out.println("  " + task.toString());
-        printLine();
-    }
-
-    public static void deleteTask(ArrayList<Task> listOfTasks, int taskNumber) {
-        if (taskNumber <= 0 || taskNumber > listOfTasks.size()) {
-            System.out.println("Invalid task number!");
-            return;
-        }
-
-        printLine();
-        System.out.println("Noted. I've removed this task:");
-        System.out.println("  " + listOfTasks.get(taskNumber - 1).toString());
-        listOfTasks.remove(taskNumber - 1);
-        System.out.println(" Now you have " + listOfTasks.size() + " tasks in the list.");
-        printLine();
-    }
-
-
-    public static void addPrintTask(ArrayList<Task> listOfTasks, Task task) throws MissingDescriptionException {
-        if (task.getDescription().isEmpty()) {
-            throw new MissingDescriptionException("Description for the tasks cannot be empty.");
-        }
-        listOfTasks.add(task);
-
-        printLine();
-        System.out.println(" Got it. I've added this task:");
-        System.out.println("  " + task.toString());
-        System.out.println(" Now you have " + listOfTasks.size() + " tasks in the list.");
-        printLine();
-    }
-    public static void printLine() {
-        System.out.println("____________________________________________________________");
-    }
-
-    private static void EnsureDirectoryExists() {
-        File dataDirectory = new File("./data");
-        if (!dataDirectory.exists()) {
-            dataDirectory.mkdirs();
-        }
-    }
-
-    private static void saveTasksToFile() {
-        try (FileWriter writer = new FileWriter(FILE_PATH)) {
-            for (Task t : listOfTasks) {
-                writer.write(t.toFileFormat() + "\n");
-            }
-        } catch (IOException e) {
-            System.out.println("Error saving tasks to file: " + e.getMessage());
-        }
-    }
-
-    private static void loadTasksFromFile() {
-        Path path = Paths.get(FILE_PATH);
-        if (!Files.exists(path)) {
-            return;
-        }
-        try {
-            Files.lines(path).forEach(line -> {
-                Task task = Task.fromFileFormat(line);
-                if (task != null) {
-                    listOfTasks.add(task);
-                }
-            });
-        } catch (IOException e) {
-            System.out.println("Error loading tasks from file: " + e.getMessage());
-        }
-    }
-
 }
